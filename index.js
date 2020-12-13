8f2f9292b7ef0cae4b9af1c9d71b0f81d79d7832
KavehKarami <kaveh.karami.kk@gmail.com>
2020-12-13 17:16:18 +0330

    JS Composition

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+function monster({ name }) {
+  return {
+    attack: () => console.log(`${name} attacked`),
+    walk: () => console.log(`${name} walked`)
+  };
+}
+
 function swimmer({ name }) {
   return {
     swim: () => console.log(`${name} swam`)
   };
 }
+
+function flayer({ name }) {
+  return {
+    fly: () => console.log(`${name} flew`)
+  };
+}
+
+function swimingFlyingMonster(name) {
+  const monsterName = { name };
+
+  return {
+    ...monster(monsterName),
+    ...swimmer(monsterName),
+    ...flayer(monsterName)
+  };
+}
+
+const obj = swimingFlyingMonster("Monster");
+obj.attack();
+obj.walk();
+obj.swim();
+obj.fly();
